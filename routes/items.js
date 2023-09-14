ac5aa89e8f61790122207b142c7e3e5846bb46d7
kimhoang115 <hoangtran@dosiinvn.com>
2023-09-14 17:03:47 +0700

    first commit

---
+++ routes/items.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('pages/items/list', { pageTitle: 'Item List Page' });
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('pages/items/add', { pageTitle: 'Add Item Page' });
+});
+
+
+module.exports = router;
