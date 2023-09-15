00930f93093d43a3493257516d4aa1b40759b967
kimhoang115 <hoangtran@dosiinvn.com>
2023-09-15 09:44:28 +0700

    u

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', require('./routes/index'));
-app.use('/admin/dashboard', require('./routes/dashboard'));
-app.use('/admin/items', require('./routes/items'));
+app.use('/admin', require('./routes/admin/home'));
+app.use('/admin/dashboard', require('./routes/admin/dashboard'));
+app.use('/admin/items', require('./routes/admin/items'));
 
 
 // catch 404 and forward to error handler
